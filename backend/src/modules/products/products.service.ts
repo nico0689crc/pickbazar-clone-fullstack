5c5ecde95cb693006515cc8124ccf6bb175730e1
Nicolas Fernandez <nico.06.89crc@gmail.com>
2024-03-18 09:24:33 +0800

    Testing workflow

--- backend/src/modules/products/products.service.ts
+++ backend/src/modules/products/products.service.ts
@@ -9,7 +9,7 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return `This action returns all products -- Updated`;
   }
 
   findOne(id: number) {
